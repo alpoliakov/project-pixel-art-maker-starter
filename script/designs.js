70630c587ec0ca929b8e993fa296dc46796ebe62
Aleksandr Poliakov <alpoliakov73@gmail.com>
2017-11-10 17:56:33 +0200

    Switching from 'id' to 'classes'

--- script/designs.js
+++ script/designs.js
@@ -1,12 +1,12 @@
 // Set the initial values and avoid global variables
 $(function() {
   // Select color input
-  const inputColor = $('#colorPicker');
+  const inputColor = $('.colorPicker');
   let color = inputColor.val();
 
   inputColor.on('blur', () => (color = inputColor.val()));
 
-  let table = $('#pixel_canvas');
+  let table = $('.pixel_canvas');
 
   // The event handler for clicking on "submit"
   const makeGrid = evt => {
@@ -14,8 +14,8 @@ $(function() {
     if (el.type !== 'submit') return;
 
     // Select size input
-    let height = $('#input_height').val();
-    let width = $('#input_width').val();
+    let height = $('.input_height').val();
+    let width = $('.input_width').val();
 
     // Clean the "old" table
     table.children().remove();
@@ -33,7 +33,7 @@ $(function() {
   };
 
   // When size is submitted by the user, call makeGrid()
-  $('#sizePicker').click(makeGrid);
+  $('.sizePicker').click(makeGrid);
 
   // Handler for "drawing"
   table.click(evt => $(evt.target).css('background', color));
