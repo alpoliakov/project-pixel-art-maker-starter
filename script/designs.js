066f5ee215666dfea366048779c04495bdb82ad5
Aleksandr Poliakov <alpoliakov73@gmail.com>
2017-11-10 16:20:37 +0200

    Code modification - use of syntax ES6

--- script/designs.js
+++ script/designs.js
@@ -1,44 +1,40 @@
 // Set the initial values and avoid global variables
-$(function(){
-    // Select color input
-    const inputColor = $('#colorPicker');
-    let color = inputColor.val();
+$(function() {
+  // Select color input
+  const inputColor = $('#colorPicker');
+  let color = inputColor.val();
 
-    inputColor.on('blur', function() {
-        color = inputColor.val();
-    });
+  inputColor.on('blur', () => color = inputColor.val());
 
-    let table = $('#pixel_canvas');
+  let table = $('#pixel_canvas');
 
-    // The event handler for clicking on "submit"
-    const makeGrid = (evt) => {
-        const el = evt.target;
-        if (el.type !== 'submit') return;
+  // The event handler for clicking on "submit"
+  const makeGrid = evt => {
+    const el = evt.target;
+    if (el.type !== 'submit') return;
 
-        // Select size input
-        let height = $('#input_height').val();
-        let width = $('#input_width').val();
+    // Select size input
+    let height = $('#input_height').val();
+    let width = $('#input_width').val();
 
-        // Clean the "old" table
-        table.children().remove();
+    // Clean the "old" table
+    table.children().remove();
 
-        // Draw the table
-        let col = '';
-        for (let i = 0; i < width; i++) {
-            col += '<td></td>';
-        }
-        for (let count = 0; count < height; count++) {
-            table.append(`<tr>${col}</tr>`);
-        }
+    // Draw the table
+    let col = '';
+    for (let i = 0; i < width; i++) {
+      col += '<td></td>';
+    }
+    for (let count = 0; count < height; count++) {
+      table.append(`<tr>${col}</tr>`);
+    }
 
-        return false
-    };
+    return false;
+  };
 
-    // When size is submitted by the user, call makeGrid()
-    $('#sizePicker').click(makeGrid);
+  // When size is submitted by the user, call makeGrid()
+  $('#sizePicker').click(makeGrid);
 
-    // Handler for "drawing"
-    table.click((evt) => {
-        $( evt.target ).css( 'background', color );
-    });
+  // Handler for "drawing"
+  table.click(evt => $(evt.target).css('background', color));
 });
