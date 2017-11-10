4096db59e7f5edfeb02b546d358504e95427bce3
Aleksandr Poliakov <alpoliakov73@gmail.com>
2017-11-10 16:06:40 +0200

    The first working version with the use of jQuery

--- script/designs.js
+++ script/designs.js
@@ -1,10 +1,44 @@
-// Select color input
-// Select size input
+// Set the initial values and avoid global variables
+$(function(){
+    // Select color input
+    const inputColor = $('#colorPicker');
+    let color = inputColor.val();
 
-// When size is submitted by the user, call makeGrid()
+    inputColor.on('blur', function() {
+        color = inputColor.val();
+    });
 
-function makeGrid() {
+    let table = $('#pixel_canvas');
 
-// Your code goes here!
+    // The event handler for clicking on "submit"
+    const makeGrid = (evt) => {
+        const el = evt.target;
+        if (el.type !== 'submit') return;
 
-}
+        // Select size input
+        let height = $('#input_height').val();
+        let width = $('#input_width').val();
+
+        // Clean the "old" table
+        table.children().remove();
+
+        // Draw the table
+        let col = '';
+        for (let i = 0; i < width; i++) {
+            col += '<td></td>';
+        }
+        for (let count = 0; count < height; count++) {
+            table.append(`<tr>${col}</tr>`);
+        }
+
+        return false
+    };
+
+    // When size is submitted by the user, call makeGrid()
+    $('#sizePicker').click(makeGrid);
+
+    // Handler for "drawing"
+    table.click((evt) => {
+        $( evt.target ).css( 'background', color );
+    });
+});
